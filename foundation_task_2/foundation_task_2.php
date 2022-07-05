0252ec3cc99115b2eea7784d770f80f8b664ee2d
Liam Maeder <liam.maeder@stratusolve.com>
2022-07-05 10:26:10 +0200

    Foundation Task 2
    - Updated the internal method to be dynamic instead of hard coded.

--- foundation_task_2/foundation_task_2.php
+++ foundation_task_2/foundation_task_2.php
@@ -1,16 +1,41 @@
 <?php
-function printFibonacciSequence($firstNumInt, $secondNumInt) {
-    if ($firstNumInt < 34) {
-        echo $firstNumInt.", ";
-        $newNumInt = $firstNumInt + $secondNumInt;
-        $firstNewNumInt = $secondNumInt;
-        $secondNewNumInt = $newNumInt;
-        printFibonacciSequence($firstNewNumInt, $secondNewNumInt);
-    } else if ($firstNumInt === 34) {
-        //Added this additional else if to remove the "," at the ens of the print
-        echo $firstNumInt;
+function printFibonacciSequence($FirstNumInt, $SecondNumInt, $GoalNumberInt) {
+
+    $EchoNumberStr = strval($FirstNumInt);
+
+    if ($FirstNumInt > 0) {
+        echo ", ".$EchoNumberStr;
+    } else {
+        echo $EchoNumberStr;
     }
+
+    if ($FirstNumInt < $GoalNumberInt) {
+        $NewNumInt = $FirstNumInt + $SecondNumInt;
+        $FirstNewNumInt = $SecondNumInt;
+        $SecondNewNumInt = $NewNumInt;
+        printFibonacciSequence($FirstNewNumInt, $SecondNewNumInt, $GoalNumberInt);
+    }
+     
+    // else if ($firstNumInt === 34) {
+    //     //Added this additional else if to remove the "," at the end of the print
+    //     echo $firstNumInt;
+    // }
 }
 
-printFibonacciSequence(0, 1);
+printFibonacciSequence(0, 1, 10000);
 ?>
+
+<!-- <?php
+// function printFibonacciSequence() {
+//     $x = 0;
+//     $y = 1;
+//     while ($x <= 34) {
+//         echo $x." ";
+//         $z = $x + $y;
+//         $x = $y;
+//         $y = $z;
+//     }
+// }
+
+// printFibonacciSequence();
+?> -->
